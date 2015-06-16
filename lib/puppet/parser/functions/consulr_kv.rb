d13738cdb4f4f73a3639c6fbc6243ac1af8518fa
aj-jester <aj@a-j.me>
2015-06-16 02:45:15 -0400

    Various consulr_kv changes: * ACL token support * Pass config via hash for simplicity * Added sane defaults * Option to pass token to API * Option to ignore 404 when keys dont exist * Option to send back either entire hash or just value * Option to decode base64 value * Option to set timeout for API call * Refactor error handling * Improve error message text

--- lib/puppet/parser/functions/consulr_kv.rb
+++ lib/puppet/parser/functions/consulr_kv.rb
@@ -7,41 +7,73 @@ require 'uri'
 module Puppet::Parser::Functions
   newfunction(:consulr_kv, :type => :rvalue) do |args|
 
-    # Lets define some customizable vars
-    uri = args[0] # e.x. http://localhost:8500 (no trailing slash)
-    nodes_prefix = args[1] # <uri>/<nodes_prefix>/<facter_perfix_key> 
-    facter_prefix_key = args[2] # The prefix key unique to each instance
+    # Default config
+    default_config = {
+      'uri'           => 'http://localhost:8500',
+      'nodes_prefix'  => 'nodes',
+      'facter_prefix' => 'hostname',
+      'value_only'    => true,
+      'base64_decode' => true,
+      'ignore_404'    => true,
+      'token'         => false,
+      'timeout'       => 5,
+    }
 
-    # Lets ensure all params are passed
-    raise Puppet::ParseError, 'uri, node prefix and facter prefix key are required' if [uri, nodes_prefix, facter_prefix_key].include?(nil)
-    
+    # Required config options (for future use)
+    required_config = []
+
+    # User config
+    user_config = args.first ? args.first : Hash.new
+
+    # final config
+    config = default_config.merge(user_config)
+
+    # Missing config
+    missing_config = required_config.reject {|i| config.has_key?(i)}
+
+    raise Puppet::ParseError, "Consulr missing config: #{missing_config.join(', ')}" unless missing_config.empty?
+
     # lookupvar returns 'nil' if the fact doesn't exist...
-    prefix = lookupvar(facter_prefix_key)
+    prefix = lookupvar(config['facter_prefix'])
 
     # ...so lets raise hell if thats the case.
-    raise Puppet::ParseError, 'prefix fact not found' if prefix.nil?
+    raise Puppet::ParseError, "Consulr facter prefix not found: #{config['facter_prefix']}" if prefix.nil?
 
     consulr = Hash.new
 
     begin
-      Timeout::timeout(5) do
-        # Get all the keys with facter prefix key
-        uri = URI.parse("#{uri}/v1/kv/#{nodes_prefix}/#{prefix}?recurse")
-        response = Net::HTTP.get_response(uri)
+      Timeout::timeout(config['timeout']) do
+        # Build and parse URI
+        build_uri = "#{config['uri']}/v1/kv/#{config['nodes_prefix']}/#{prefix}?recurse"
+        build_uri << "&token=#{config['token']}" if config['token']
 
-        # If at least one key with the facter prefix is not found,
-        # fail silently
-        raise "HTTP error: #{nodes_prefix}/#{prefix}/ #{response.code} #{response.message}" unless ['200', '404'].include?(response.code)
+        response = Net::HTTP.get_response(URI.parse(build_uri))
+
+        # Following HTTP codes will not raise an exception
+        ignore_http_codes = ['200']
+
+        # Option to ignore 404
+        ignore_http_codes << '404' if config['ignore_404']
+
+        raise Puppet::ParseError, "Consulr HTTP error: #{config['uri']}/v1/kv/#{config['nodes_prefix']}/#{prefix}/ (#{response.code}: #{response.message})" unless ignore_http_codes.include?(response.code)
         data = JSON.parse(response.body) rescue []
 
         # Iterate though the keys and put them in a hash 
         data.each do |kv|
-          # Replace only the first occurence of '<nodes_prefix>/<facter_prefix_key>/'
-          # with blank so when calling in puppet we can omit the facter prefix key.
-          # For example:
-          # $consul['django_version'] instead of $consul['nodes/i-a8caf087/django_version']
-          # but this is OK: $consul['haproxy/webs/i-a8caf087/version']
-          consulr[kv['Key'].sub(/^#{nodes_prefix}\/#{prefix}\//, "")] = Base64.decode64(kv['Value'])
+
+          # We are determining 2 things here:
+          # 1) Whether to send back only value or the entire hash or
+          # 2) Whether to decode the value before sending it back
+          if config['value_only']
+            result = config['base64_decode'] ? Base64.decode64(kv['Value']) : kv['Value']
+          else
+            kv['Value'] = Base64.decode64(kv['Value']) if config['base64_decode']
+            result = kv
+          end
+
+          # Finally remove <nodes_prefix>/<prefix> from the
+          # path and assign that value as the new key
+          consulr[kv['Key'].sub(/^#{config['nodes_prefix']}\/#{prefix}\//, "")] = result
         end
       end
     rescue Timeout::Error => e
